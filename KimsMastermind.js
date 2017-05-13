268fe861947078c54c580bd245574b37e99bc6a9
pejtiry <reg@osiry.dk>
2017-05-13 18:08:56 +0200

    Add files via upload

--- KimsMastermind.js
+++ KimsMastermind.js
@@ -178,7 +178,8 @@ var evaluate = function(origCode,newCode){
     for (var i= 0; i < origC.length; i++){
         for(var k = 0; k < newC.length; k++){
             if (origC[i]===newC[k]){
-                newC.splice([k],1);
+                newC.splice([k], 1);
+                origC.splice([k], 1);
                 isColor++;
             }
         }
