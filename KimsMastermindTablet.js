f4c9ef76838729ce2614cd6c6471dcf8911015c6
pejtiry <reg@osiry.dk>
2017-05-30 20:20:19 +0200

    Update KimsMastermindTablet.js

--- KimsMastermindTablet.js
+++ KimsMastermindTablet.js
@@ -377,6 +377,12 @@ void draw() {
 //void mouseClicked() {
 //    mouseEvent(mouseX,mouseY);
 //};
-void mouseReleased() {
+//void mouseReleased() {
+//	mouseEvent(mouseX,mouseY);
+//};
+void touchend(e) {
 	mouseEvent(mouseX,mouseY);
+	for (var i = 0; i < e.changedTouches.lenth; i++){
+		mouseEvent(e.changedTouches[i].);
+	}
 };
